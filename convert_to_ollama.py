63e66553f8a4a43115ead06adf023051842236e0
Abdula3469 <152958631+Abdula3469@users.noreply.github.com>
2026-03-26 19:51:10 +0300

    convert_to_ollama.py

--- convert_to_ollama.py
+++ convert_to_ollama.py
@@ -2,19 +2,18 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 import os
-
+#Тут загружается базовая модель, загружается на кпу
 base_model = AutoModelForCausalLM.from_pretrained(
     "microsoft/phi-2",
     torch_dtype=torch.float16,
     device_map="cpu",
     trust_remote_code=True
 )
-
+#Тут загружается лора адаптер, он содержит только изменения
 model = PeftModel.from_pretrained(base_model, "./phi2-sparql-lora-final")
-
-print("СЛИЯНИЕ ГУРРЕН-ЛАГАНН")
+#Тут же я складываю веса базовой модели и лора адаптера, чтобы создать модель без лора слоев
 merged_model = model.merge_and_unload()
-
+#Тут же сохраняется новая модель, которая готова для конвертации в gguf, чтоьы  испоьзовать ее в оллама
 output_dir = "./phi2-sparql-merged"
 merged_model.save_pretrained(output_dir)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
