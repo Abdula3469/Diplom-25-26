916199e9c3fa336cfed38d4ecf2cf0ac09aad50a
Abdula3469 <152958631+Abdula3469@users.noreply.github.com>
2026-05-12 14:51:59 +0300

    Update Obuchenie.py

--- Obuchenie.py
+++ Obuchenie.py
@@ -8,14 +8,14 @@ import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
+    load_in_4bit=True, #Загружает модель в 4битном формате
+    bnb_4bit_quant_type="nf4", #формат 4битного квантования, точнее обычного
     bnb_4bit_compute_dtype=torch.float32,     
-    bnb_4bit_use_double_quant=True,
+    bnb_4bit_use_double_quant=True, #Двойное квантование для дополнительной экономии места
 )
 
 
-
+#Загрузка токенизатора, который превращает текст в числа
 model_name = "Qwen/Qwen2-1.5B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(
     model_name,
@@ -30,27 +30,28 @@ stop_tokens = ["###", "User:", "Assistant:", "\n\n\n"]
 tokenizer.eos_token = tokenizer.eos_token
 tokenizer.pad_token = tokenizer.eos_token
 
-print("Загрузка модели")
+#Тут происходит загрузка модели,
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    quantization_config=bnb_config,
-    device_map="auto",                        
+    quantization_config=bnb_config, #применяет квантование
+    device_map="auto", #автоматически выбирает куда загружаться, на cpu или gpu                
     trust_remote_code=True,
     torch_dtype=torch.float32,              
 )
 
 
 lora_config = LoraConfig(
-    r=64,                  
-    lora_alpha=128,           
-    lora_dropout=0.1,
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+    r=64,   #Количество дополнительных параметров, чем этот параметр выше, тем точнее               
+    lora_alpha=128,   #коэффицент масштабирования, желательно, чтобы было в 2 раз больше чем количество доп. параметров        
+    lora_dropout=0.1, #Отключает часть, а точнее 10% нейронов для регуляризации
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"], #слои внимания, точнее Запрос, Ключ, Значение и Выход
     bias="lora_only",
 )
 
 model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
+#Тут происходит загрузка и форматирование данных
 def load_and_format_data(file_path):
     formatted_data = []
     
@@ -66,17 +67,17 @@ def load_and_format_data(file_path):
             formatted_data.append({"text": full_text})
     
     return formatted_data
-
+#Токенизация
 def tokenize_function(examples):
     tokenized = tokenizer(
         examples["text"],
-        truncation=True,
-        padding="max_length",
-        max_length=512,
+        truncation=True, #Обрезает текст, который длиннее 512 токенов
+        padding="max_length", #Добавляет паддинг до 512 токенов
+        max_length=512, #Максимальная длина последовательности
         return_tensors=None,
     )
     
-    tokenized["labels"] = tokenized["input_ids"].copy()
+    tokenized["labels"] = tokenized["input_ids"].copy() #Указывает какой токен правильный
     
     return tokenized
 
@@ -93,27 +94,27 @@ data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,
 )
-
+#Аргументы обучения
 training_args = TrainingArguments(
     output_dir="./qwen2-sparql-lora",
-    num_train_epochs=5,
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=8,
-    learning_rate=2e-4,
-    warmup_steps=50,
-    warmup_ratio=0.1,
-    logging_steps=10,
-    save_steps=200,
-    save_total_limit=3,
-    fp16=True,
-    gradient_checkpointing=True,
-    remove_unused_columns=False,
-    weight_decay=0.01,
-    dataloader_pin_memory=False,
-    report_to="none",
-    max_grad_norm=0.5,
+    num_train_epochs=5, #Модель 5 раз пройдёт по всем данным, чтобы повысить качество
+    per_device_train_batch_size=1, #размер батча, ввиду экономии очень маленький
+    gradient_accumulation_steps=8, #накопление градиентов, эффективный батч
+    learning_rate=2e-4, #стандартная скорость обучения
+    warmup_steps=50, 
+    warmup_ratio=0.1, #штрафует за большие веса
+    logging_steps=10, 
+    save_steps=200, 
+    save_total_limit=3, 
+    fp16=True, #Использует float16 для ускорения
+    gradient_checkpointing=True, #Экономит память
+    remove_unused_columns=False, 
+    weight_decay=0.01, 
+    dataloader_pin_memory=False, 
+    report_to="none", 
+    max_grad_norm=0.5, #Обрезает градиенты
 )
-
+#Запуск обучения
 trainer = Trainer(
     model=model,
     args=training_args,
