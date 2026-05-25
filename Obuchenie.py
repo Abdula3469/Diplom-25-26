a203690a7a54b098a3cace02e0ac95d4f577d45a
Abdula3469 <152958631+Abdula3469@users.noreply.github.com>
2026-05-25 10:29:22 +0300

    Update Obuchenie.py

--- Obuchenie.py
+++ Obuchenie.py
@@ -41,8 +41,8 @@ model = AutoModelForCausalLM.from_pretrained(
 
 
 lora_config = LoraConfig(
-    r=64,   #Количество дополнительных параметров, чем этот параметр выше, тем точнее               
-    lora_alpha=128,   #коэффицент масштабирования, желательно, чтобы было в 2 раз больше чем количество доп. параметров        
+    r=32,   #Количество дополнительных параметров, чем этот параметр выше, тем точнее               
+    lora_alpha=64,   #коэффицент масштабирования, желательно, чтобы было в 2 раз больше чем количество доп. параметров        
     lora_dropout=0.1, #Отключает часть, а точнее 10% нейронов для регуляризации
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj"], #слои внимания, точнее Запрос, Ключ, Значение и Выход
     bias="lora_only",
@@ -97,22 +97,22 @@ data_collator = DataCollatorForLanguageModeling(
 #Аргументы обучения
 training_args = TrainingArguments(
     output_dir="./qwen2-sparql-lora",
-    num_train_epochs=5, #Модель 5 раз пройдёт по всем данным, чтобы повысить качество
+    num_train_epochs=12, #Модель 5 раз пройдёт по всем данным, чтобы повысить качество
     per_device_train_batch_size=1, #размер батча, ввиду экономии очень маленький
     gradient_accumulation_steps=8, #накопление градиентов, эффективный батч
-    learning_rate=2e-4, #стандартная скорость обучения
+    learning_rate=1e-4, #стандартная скорость обучения
     warmup_steps=50, 
-    warmup_ratio=0.1, #штрафует за большие веса
+    warmup_ratio=0.15, #штрафует за большие веса
     logging_steps=10, 
     save_steps=200, 
     save_total_limit=3, 
     fp16=True, #Использует float16 для ускорения
     gradient_checkpointing=True, #Экономит память
     remove_unused_columns=False, 
-    weight_decay=0.01, 
+    weight_decay=0.05, 
     dataloader_pin_memory=False, 
     report_to="none", 
-    max_grad_norm=0.5, #Обрезает градиенты
+    max_grad_norm=0.3, #Обрезает градиенты
 )
 #Запуск обучения
 trainer = Trainer(
